257b04cfbd18e63aa84d0e18f189dbc66b5f16ec
brandon-wilburn <36947865+brandon-wilburn@users.noreply.github.com>
2019-12-10 20:56:12 -0600

    1.0

---
+++ Web/mainJS.js
@@ -0,0 +1,39 @@
+var dS = document.getElementsByClassName("drive-sans")[0];
+dS.addEventListener("click", dSLink, false);
+
+function dSLink() {
+    console.log("Button works!");
+    window.open("drive-sans.html", "_self");
+}
+
+var rF = document.getElementsByClassName("rain-fall")[0];
+rF.addEventListener("click", rFLink, false);
+
+function rFLink() {
+    console.log("Button works!");
+    window.open("rain-fall.html", "_self");
+}
+
+var sS = document.getElementsByClassName("chameleon")[0];
+sS.addEventListener("click", sSLink, false);
+
+function sSLink() {
+    console.log("Button works!");
+    window.open("chameleon.html", "_self");
+}
+
+var tE = document.getElementsByClassName("the-end")[0];
+tE.addEventListener("click", tELink, false);
+
+function tELink() {
+    console.log("Button works!");
+    window.open("the-end.html", "_self");
+}
+
+var tG = document.getElementsByClassName("the-groove")[0];
+tG.addEventListener("click", tGLink, false);
+
+function tGLink() {
+    console.log("Button works!");
+    window.open("the-groove.html", "_self");
+}
